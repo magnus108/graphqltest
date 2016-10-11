8f667cf054e831643b7921d06b6316579f1f389d
Magnus Møller <magnus108@me.com>
2016-10-11 21:47:36 +0200

    deploy.shfinal

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASSWORD
-docker push magnus108docker/graphqltest:$CIRCLE_SHA1
+docker push magnus108docker/graphqltest
 
 ssh ubuntu@52.57.165.69 << EOF
 docker pull magnus108docker/graphqltest:latest
-#docker stop web || true
-#docker rm web || true
-#docker rmi willrstern/sample-node:current || true
-#docker tag willrstern/sample-node:latest willrstern/sample-node:current
-
-docker run -d --restart always --name graphqltest -p 80:3000 magnus108docker/graphqltest:latest
-
+docker stop graphqltest || true
+docker rm graphqltest || true
+docker rmi magnus108docker/graphqltest:current || true
+docker tag magnus108docker/graphqltest:latest magnus108docker/graphqltest:current
+docker run -d --restart always --name graphqltest -p 80:3000 magnus108docker/graphqltest:current
 EOF
