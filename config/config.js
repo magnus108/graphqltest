a00aabf0b5edd52b2c16f48f8a98a3b8ccf2b07c
Magnus Møller <magnus108@me.com>
2016-10-15 01:59:23 +0200

    some smallfixes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  "development": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_NAME,
-    "host": process.env.POSTGRES_ADDR,
-    "dialect": "postgres"
-  }
+  "username": process.env.POSTGRES_USER,
+  "password": process.env.POSTGRES_PASSWORD,
+  "database": process.env.POSTGRES_NAME,
+  "host": process.env.POSTGRES_ADDR,
+  "dialect": "postgres"
 };
